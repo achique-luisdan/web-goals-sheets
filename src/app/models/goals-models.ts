c36f80074c7fe9ebbb4e810e0b74139b1eb06d04
achique-luisdan <achique.luisdan@gmail.com>
2021-12-07 16:03:48 -0400

    Fix al guardar ESLint"

---
+++ src/app/models/goals-models.ts
@@ -0,0 +1,11 @@
+/**
+ * Descripción: Módulo App.
+ * Autor: achique-luisdan
+ * Fecha: 07-12-2021
+ */
+export class PeriodicElement {
+  name: string = '';
+  position: number = 0;
+  weight: number = 0;
+  symbol: string = '';
+}
